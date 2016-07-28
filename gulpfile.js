3b94065312d52b9ee3343e37d061d9af07184e98
Håvard Vidme <havard.vidme@gmail.com>
2016-07-28 14:01:45 +0200

    Added Pug for markup templating

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,27 @@
 
 var connect = require('gulp-connect');
 var gulp = require('gulp');
+var pug = require('gulp-pug');
 
 gulp.task('connect', function () {
   connect.server();
 });
 
-gulp.task('default', ['connect'], function () {
+gulp.task('pug-build', function () {
+  gulp.src('src/pug/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('build/'));
+});
+gulp.task('pug-dist', function () {
+  gulp.src('src/pug/*.pug')
+    .pipe(pug({
+      pretty: false
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['pug-build', 'pug-dist', 'connect'], function () {
+  gulp.watch('src/pug/**/*.pug', ['pug-build', 'pug-dist']);
 });
