7bde0c2c1400319b4b4774038c4a7d24a00415b3
Håvard Vidme <havard.vidme@gmail.com>
2016-07-28 14:40:16 +0200

    Added component files to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
 'use strict';
 
+var prefixUrls = function (arr, prefix) {
+  for (var i = 0, j = arr.length; i < j; i++) {
+    arr[i] = prefix + arr[i];
+  }
+  return arr;
+};
+
 var connect = require('gulp-connect');
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+
+var config = {
+  vendorjs: [
+    'bower_components/lodash/dist/lodash.min.js',
+    'bower_components/react/react.min.js',
+    'bower_components/react/react-dom.min.js'
+  ],
+  vendorcss: [
+    'bower_components/bootstrap/dist/css/bootstrap.min.css'
+  ]
+}
 
 gulp.task('connect', function () {
   connect.server();
 });
 
 gulp.task('pug-build', function () {
+  var locals = {
+    styles: prefixUrls(config.vendorcss, '../'),
+    scripts: prefixUrls(config.vendorjs, '../')
+  };
   gulp.src('src/pug/*.pug')
     .pipe(pug({
+      locals: locals,
       pretty: true
     }))
     .pipe(gulp.dest('build/'));
