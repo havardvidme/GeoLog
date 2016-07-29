b85b3e423f4eb78e66de3b0f38c821535d1ea2c6
Håvard Vidme <havard.vidme@gmail.com>
2016-07-29 10:21:13 +0200

    Created a basic structure with button and list

--- dist/js/local.js
+++ dist/js/local.js
@@ -1,11 +1,89 @@
-var GeoLog = React.createClass({
-  displayName: "GeoLog",
+var POSITION_TEMPLATE = Object.assign({}, {
+  id: 0,
+  description: '',
+  latitude: 0.0,
+  longitude: 0.0,
+  timestamp: 0
+});
 
+var GeoLog = React.createClass({
+  displayName: 'GeoLog',
+
+  propTypes: {
+    onClick: React.PropTypes.func.isRequired
+  },
+  getInitialState: function () {
+    return {
+      id: 0,
+      position: POSITION_TEMPLATE,
+      positions: []
+    };
+  },
+  /*componentDidMount: function () {
+    var positions = [];
+    this.setState({
+      positions: positions
+    });
+  },*/
   render: function () {
+    var positionItems = [];
+    _.orderBy(this.state.positions, ['timestamp'], ['desc']).forEach(function (position) {
+      positionItems.push(React.createElement(
+        'a',
+        { className: 'list-group-item', href: '#' },
+        position.timestamp
+      ));
+    });
+
     return React.createElement(
-      "div",
+      'div',
       null,
-      "React is running!"
+      React.createElement(
+        'div',
+        { className: 'container' },
+        React.createElement(
+          'div',
+          { className: 'page-header' },
+          React.createElement(
+            'h1',
+            null,
+            'GeoLog'
+          )
+        ),
+        React.createElement(
+          'button',
+          { className: 'btn btn-lg btn-success btn-block', onClick: this.addPosition },
+          'Add position'
+        ),
+        React.createElement('hr', null),
+        React.createElement(
+          'div',
+          { className: 'list-group' },
+          positionItems
+        )
+      )
     );
+  },
+
+  addPosition: function (event) {
+    navigator.geolocation.getCurrentPosition(this.getPositionSuccess, this.getPositionError);
+  },
+  getPositionSuccess: function (position) {
+    var id = this.state.id + 1;
+    var position = Object.assign({}, this.state.position, {
+      id: id,
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      timestamp: position.timestamp
+    });
+    var positions = this.state.positions;
+    positions.push(position);
+    this.setState({
+      id: id,
+      positions: positions
+    });
+  },
+  getPositionError: function (error) {
+    console.log('code: %s, message: %s', error.code, error.message);
   }
 });
--- src/react/GeoLog.js
+++ src/react/GeoLog.js
@@ -1,7 +1,64 @@
+var POSITION_TEMPLATE = Object.assign({}, {
+  id: 0,
+  description: '',
+  latitude: 0.0,
+  longitude: 0.0,
+  timestamp: 0
+});
+
 var GeoLog = React.createClass({
+  propTypes: {
+    onClick: React.PropTypes.func.isRequired
+  },
+  getInitialState: function () {
+    return {
+      id: 0,
+      position: POSITION_TEMPLATE,
+      positions: []
+    };
+  },
   render: function () {
+    var positionItems = [];
+    _.orderBy(this.state.positions, ['timestamp'], ['desc']).forEach(function (position) {
+      positionItems.push(
+        <a className="list-group-item" href="#">
+          {position.timestamp}
+        </a>
+      );
+    });
     return (
-      <div>React is running!</div>
+      <div>
+        <div className="container">
+          <div className="page-header">
+            <h1>GeoLog</h1>
+          </div>
+          <button className="btn btn-lg btn-success btn-block" onClick={this.addPosition}>Add position</button>
+          <hr />
+          <div className="list-group">{positionItems}</div>
+        </div>
+      </div>
     );
+  },
+
+  addPosition: function (event) {
+    navigator.geolocation.getCurrentPosition(this.getPositionSuccess, this.getPositionError);
+  },
+  getPositionSuccess: function(position) {
+    var id = this.state.id + 1;
+    var position = Object.assign({}, this.state.position, {
+      id: id,
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      timestamp: position.timestamp
+    });
+    var positions = this.state.positions;
+    positions.push(position);
+    this.setState({
+      id: id,
+      positions: positions
+    });
+  },
+  getPositionError: function(error) {
+    console.log('code: %s, message: %s', error.code, error.message);
   }
 });
